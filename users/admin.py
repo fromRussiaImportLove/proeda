fc321e4896e3701a52ced67aa92c06721ec65ce7
Muffin Knight <muffin@lukojo.com>
2020-11-26 02:16:58 +0300

    clean mess

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,8 @@ from users.models import CustomUser
 
 
 class CustomUsersAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'username', 'email', 'is_banned', 'is_active','role')
+    list_display = ('pk', 'username', 'email', 'is_banned',
+                    'is_active', 'role')
     list_filter = ('username', 'email')
     empty_value_display = '-пусто-'
 
