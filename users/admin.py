8f5345616f7c59614e33f8477a89c5ff3f8f6218
Muffin Knight <muffin@lukojo.com>
2020-11-26 19:07:53 +0300

    change filter to search in users admin panel

--- users/admin.py
+++ users/admin.py
@@ -6,7 +6,9 @@ from users.models import CustomUser
 class CustomUsersAdmin(admin.ModelAdmin):
     list_display = ('pk', 'username', 'email', 'is_banned',
                     'is_active', 'role')
-    list_filter = ('username', 'email')
+    list_filter = ('role', 'is_banned')
+    list_display_links = ('pk', 'username')
+    search_fields = ('username', 'email', 'first_name', 'second_name')
     empty_value_display = '-пусто-'
 
 
