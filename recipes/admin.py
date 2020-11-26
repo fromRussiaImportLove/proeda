a1e222162d66c4ee2453e886add5528d6c8fc732
Muffin Knight <muffin@lukojo.com>
2020-11-26 19:07:12 +0300

    register model in admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import (Recipe, Unit, Ingredient, Tag,
+    IngredientsInRecipe, Follow, Favorite, Basket)
+
+
+admin.site.empty_value_display = '-пусто-'
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'author', 'ingredients')
+    date_hierarchy = 'pub_date'
+
+
+@admin.register(Unit, Ingredient)
+class SimpeAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    ordering = ('name', )
+
+
+@admin.register(IngredientsInRecipe)
+class IngredientsInRecipeAdmin(admin.ModelAdmin):
+    list_display_links = ('recipe', 'ingredient')
+    list_display = ('recipe', 'ingredient', 'amount')
+    search_fields = ('recipe', 'ingredient')
+
+
+@admin.register(Follow, Favorite, Basket)
+class SimpeStrAdmin(admin.ModelAdmin):
+    list_display_links = ('__str__', )
+    list_display = ('pk', '__str__')
+    list_filter = ('user', )
