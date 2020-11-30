5853136a983c20201cd857ef88ec2f77433f13e4
Muffin Knight <muffin@lukojo.com>
2020-11-30 04:13:50 +0300

    fix paginator

--- recipes/views.py
+++ recipes/views.py
@@ -5,8 +5,9 @@ from django.views.decorators.cache import cache_page
 
 from recipes.models import Recipe
 
-def get_paginator_context(posts_list, page_slice, request) -> dict:
-    paginator = Paginator(posts_list, 10)
+
+def get_paginator_context(objects_list, page_slice, request) -> dict:
+    paginator = Paginator(objects_list, page_slice)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
     context = {'page': page, 'paginator': paginator}
@@ -24,6 +25,6 @@ def server_error(request):
 
 # @cache_page(20 * 60)
 def index(request):
-    posts_list = Recipe.objects.all()
-    context = get_paginator_context(posts_list, 10, request)
+    recipes_list = Recipe.objects.all()
+    context = get_paginator_context(recipes_list, 9, request)
     return render(request, 'recipes/index.html', context)
