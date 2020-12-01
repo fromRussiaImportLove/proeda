a8a734f14c898f3020fd1db26393f425fbef4449
Muffin Knight <muffin@lukojo.com>
2020-12-01 22:37:37 +0300

    complete registration templates and around

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+
+class CreationForm(UserCreationForm):
+    """
+    Собственный классы для формы регистрации.
+    Наследуем не от кастомных модей, а от встроенного UserCrationForm
+    В качестве модели используем встроенную User
+    """
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2')
