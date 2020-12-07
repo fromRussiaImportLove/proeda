2cadc6e44087c2fa2785b86e1e30dc9d21cd3990
Muffin Knight <muffin@lukojo.com>
2020-12-07 04:24:33 +0300

    make tags appearance

--- front/js/components/Tags.js
+++ front/js/components/Tags.js
@@ -13,6 +13,15 @@ const tag_id = tag_location.get(tag_in_url);
 const tag_for_active = document.getElementById(tag_id);
 if (tag_for_active !== null) {
   tag_for_active.classList.add(class_to_add_to_tag);
+  console.log(tag_in_url)
+  tag_for_active.href=".";
+  const allcheckbox = document.querySelectorAll('.tags__checkbox');
+  for (var i = 0; i < allcheckbox.length; i++) {
+    if (allcheckbox[i].href.indexOf(tag_in_url) > -1) {
+    allcheckbox[i].href=".";
+    } else {
+    allcheckbox[i].classList.remove("tags__checkbox_active");
+    }
+  }
 }
-
 
